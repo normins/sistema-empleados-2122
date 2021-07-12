d2dcec500428da9644ed91ae225a77270e9395b6
normisalcedo <62967178+normins@users.noreply.github.com>
2021-07-12 18:29:11 -0300

    creación de proyecto

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='sistema2122'
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    sql ="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Roco', 'roco@gmail.com', 'roco.jpg');"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    return render_template('empleados/index.html')
+
+#punto de entrada a la aplicacion
+if __name__=='__main__':
+    app.run(debug=True)
